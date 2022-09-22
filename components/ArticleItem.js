0495c25cc8ceaa5ee175f26118da94d8da173db7
Bcypher01 <olumidebabayemi1@gmail.com>
2022-09-22 08:12:50 +0100

    Add individual article page

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -4,10 +4,10 @@ import articleStyles from "../styles/Article.module.css";
 const ArticleItem = ({ article }) => {
   return (
     <Link href="/article/[id]" as={`/article/${article.id}`}>
-      <a className={articleStyles.card}>
+      <div className={articleStyles.card}>
         <h3>{article.title} &rarr;</h3>
         <p>{article.body}</p>
-      </a>
+      </div>
     </Link>
   );
 };
